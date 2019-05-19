01021f97d26532215a2a2259ca6caec105e237a0
aircraftman3 <aircraftman3@gmail.com>
2019-05-19 12:36:23 +0200

    last commit

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-$(document).on('click', '#submit-button', function(){
+$(document).on('click', '#submit-button', function () {
   var x = document.getElementById('inlineFormInputName').value;
   $("#form2").show();
   $('#input-name').text('Welcome ' + x + '...');
 });
+
+window.onscroll = function () {
+  myFunction()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    navbar.classList.add("navbar2")
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.remove("navbar2");
+  }
+}
